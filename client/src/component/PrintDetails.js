386519c3f4c605567f35ed207b046e50d48d114b
Aditya Kumar <adityapal1409@gmail.com>
2024-12-16 22:06:48 +0530

    input images

--- client/src/component/PrintDetails.js
+++ client/src/component/PrintDetails.js
@@ -1,17 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import Layout from "../core/Layout";
-import HeaderImage from "../assets/header.PNG";
-import FooterImage from "../assets/footer.PNG";
 
 const PrintDetails = () => {
 	const canvasRef = useRef(null);
 	const [text, setText] = useState("");
-
-	const headerImage = new Image();
-	const footerImage = new Image();
-
-	headerImage.src = HeaderImage;
-	footerImage.src = FooterImage;
+	const [headerImage, setHeaderImage] = useState(null);
+	const [footerImage, setFooterImage] = useState(null);
 
 	function updateCanvas() {
 		const canvas = canvasRef.current;
@@ -21,19 +15,31 @@ const PrintDetails = () => {
 			context.fillStyle = "#000000";
 			context.font = `20px Roboto sans-serif`;
 
-			//Header Image
-			context.drawImage(headerImage, 0, 0, canvas.width, 80);
+			// Draw header image if present
+			if (headerImage) {
+				const img = new Image();
+				img.onload = () => {
+					context.drawImage(img, 0, 0, canvas.width, 80);
+				};
+				img.src = headerImage;
+			}
 
-			// Footer Image
-			context.drawImage(
-				footerImage,
-				0,
-				canvas.height - 80,
-				canvas.width,
-				80
-			);
+			// Draw footer image if present
+			if (footerImage) {
+				const img = new Image();
+				img.onload = () => {
+					context.drawImage(
+						img,
+						0,
+						canvas.height - 80,
+						canvas.width,
+						80
+					);
+				};
+				img.src = footerImage;
+			}
 
-			context.fillText(text, 50, 100);
+			context.fillText(text, 50, 150);
 		}
 	}
 
@@ -41,26 +47,63 @@ const PrintDetails = () => {
 		updateCanvas();
 	});
 
+	const handleImageUpload = (e, setImage) => {
+		const file = e.target.files[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = (event) => {
+				setImage(event.target.result); // Store the Data URL
+			};
+			reader.readAsDataURL(file);
+		}
+	};
+
 	return (
 		<Layout>
 			<h1 style={{ textAlign: "center" }}>Print Details</h1>
 			<div style={styles.container}>
-				<div style={{ width: "100%" }}>
-					<label for="text">Text:</label>
-					<input
-						id="text"
-						type="text"
-						value={text}
-						style={styles.input}
-						onChange={(e) => {
-							setText(e.target.value);
-						}}
-					/>
-				</div>
+				<form>
+					<div style={{ width: "100%" }}>
+						<label for="text">Text:</label>
+						<input
+							id="text"
+							type="text"
+							value={text}
+							style={styles.input}
+							onChange={(e) => {
+								setText(e.target.value);
+							}}
+						/>
+					</div>
+					<div style={{ width: "100%", marginTop: "10px" }}>
+						<label htmlFor="headerImage">Header Image:</label>
+						<input
+							id="headerImage"
+							type="file"
+							style={styles.input}
+							accept="image/*"
+							onChange={(e) =>
+								handleImageUpload(e, setHeaderImage)
+							}
+						/>
+					</div>
+					<div style={{ width: "100%", marginTop: "10px" }}>
+						<label htmlFor="footerImage">Footer Image:</label>
+						<input
+							id="footerImage"
+							type="file"
+							style={styles.input}
+							accept="image/*"
+							onChange={(e) =>
+								handleImageUpload(e, setFooterImage)
+							}
+						/>
+					</div>
+				</form>
 				<canvas
 					ref={canvasRef}
-					width={500}
-					height={600}
+					width={400}
+					height={500}
 					style={{
 						border: "1px solid black",
 						width: "100%",
