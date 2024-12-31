0d92effae820fd42a65f268f6c8bb35cc44145a9
khushi <khushishukla5890@gmail.com>
2024-12-31 14:37:30 +0530

    app

--- client/src/component/viewpsychodiagnosticreport.js
+++ client/src/component/viewpsychodiagnosticreport.js
@@ -179,7 +179,7 @@ const ViewPDReport = () => {
   `).join('')}
 
 <div>
-   Sugession: <p>${patientData.suggestions.map((suggestion, index)=> `${index} ${suggestion}`).join(',')}</p>
+   Sugession: <p>${patientData.suggestions.map((suggestion, index)=> `${index +1} ${suggestion}`).join(' , ')}</p>
 </div>
 </div>
   `
